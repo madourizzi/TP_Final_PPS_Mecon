27cf79b2677fe77abe53ad832d864c2fb00616ba
luloop <lucilarizzi@gmail.com>
2019-10-26 09:11:38 -0300

    nuevo servicio del archivos con servicio y storage firebase

--- Comanda/src/app/pages/admin/admin.page.ts
+++ Comanda/src/app/pages/admin/admin.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SpinnerService } from 'src/app/services/spinner.service';
 import { flatMap } from 'rxjs/operators';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { ArchivosService } from 'src/app/services/archivos.service';
 
 @Component({
   selector: 'app-admin',
@@ -15,38 +16,45 @@ export class AdminPage implements OnInit {
   cargarProducto;
   botonera;
 
-  constructor( private spinner: SpinnerService, private qr: BarcodeScanner) {
-    this.adminPerfilUser=false;
-    this.title=" administrador";
+  constructor(private spinner: SpinnerService,
+    private qr: BarcodeScanner,
+    private archivos: ArchivosService) {
+    this.adminPerfilUser = false;
+    this.title = " administrador";
   }
-  
+
   ngOnInit() {
-    setTimeout(()=>    this.spinner.hide(), 1000  );
-    this.cargarProducto=false;
-    this.botonera=true;
+    setTimeout(() => this.spinner.hide(), 1000);
+    this.cargarProducto = false;
+    this.botonera = true;
   }
 
 
-  cambiarPerfilUsuario()
-  {
-    this.adminPerfilUser= true;
-    this.botonera=false;
-    this.cargarProducto= false;
+  cambiarPerfilUsuario() {
+    this.adminPerfilUser = true;
+    this.botonera = false;
+    this.cargarProducto = false;
   }
 
 
-  cargarProductos()
-  {
-    this.cargarProducto= true;
-    this.adminPerfilUser= false;
-    this.botonera=false;
+  cargarProductos() {
+    this.cargarProducto = true;
+    this.adminPerfilUser = false;
+    this.botonera = false;
 
   }
 
-  leerQr()
-  {
- console.log("qr"+  this.qr.scan());
+  leerQr() {
+    console.log("qr" + this.qr.scan());
 
+    /* ionic cordova plugin add phonegap-plugin-barcodescanner
+   npm install @ionic-native/barcode-scanner */
+  }
+
+  camara() {
+    this.archivos.camara('producto');
+    /* ionic cordova plugin add cordova-plugin-file
+    npm install @ionic-native/file */
 
   }
 
